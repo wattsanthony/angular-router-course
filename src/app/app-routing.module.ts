7a20c30250d54f5f489e57499306bd049bf6c5e0
awatts30 <anthony.watts@dxc.com>
2024-01-12 17:36:51 +1030

    Initial router setup for 2 pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,27 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
-
+    // Login path to component
+    {
+      path: "login",
+      component: LoginComponent
+    },
+    // About path to component
+    {
+      path: "about",
+      component: AboutComponent
+    }
 ];
 
 @NgModule({
   imports: [
-
+    RouterModule.forRoot(routes) // Imports the routes array from above
   ],
-  exports: [RouterModule],
+  exports: [RouterModule], // Export module for use
   providers: [
 
   ]
